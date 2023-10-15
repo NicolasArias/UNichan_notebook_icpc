f7d06ac5a03ee80021a7e5b9d28077a562d62432
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2023-10-14 20:43:47 -0500

    Create sol.cpp

---
+++ Utilities/template/sol.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+
+int main(){
+  #ifdef LOCAL
+    freopen("in","r", stdin);
+    freopen("out","w",stdout);
+  #endif
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	return 0;
+}
