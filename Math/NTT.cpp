dc933ed71b80dfade51e00fa0366be417ce89289
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2023-10-20 09:45:17 -0500

    Update NTT.cpp

--- Math/NTT.cpp
+++ Math/NTT.cpp
@@ -32,24 +32,30 @@ int find_primitive_root(int mod) {
   assert(false);
 }
 
-void fft(vector<int> &a, bool inv = 0) {
+void fft(vector<int> & a, bool invert) {
   int n = a.size();
-  for(int i = 1, j = 0; i < n; i++) {
-    int c = n >> 1;
-    for (; j >= c; c >>= 1) j -= c;
-    j += c;
-    if(i < j) swap(a[i], a[j]);
+
+  for (int i = 1, j = 0; i < n; i++) {
+    int bit = n >> 1;
+    for (; j & bit; bit >>= 1)
+      j ^= bit;
+      j ^= bit;
+
+      if (i < j)
+        swap(a[i], a[j]);
   }
+
   for (int len = 2; len <= n; len <<= 1) {
-    int wlen = inv ? root_1 : root;
-    for(int i = len; i < root_pw; i <<= 1) wlen = (1 LL * wlen * wlen) % mod;
-    for(int i = 0; i < n; i += len) {
+    int wlen = invert ? root_1 : root;
+    for (int i = len; i < root_pw; i <<= 1)
+      wlen = (int)(1LL * wlen * wlen % mod);
+    for (int i = 0; i < n; i += len) {
       int w = 1;
-      for(int j = 0; j < (len >> 1); j++) {
-        int u = a[i + j], v = (a[i + j + (len >> 1)] * 1 LL * w) % mod;
-        a[i + j] = u + v < mod ? u + v : u + v - mod;
-        a[i + j + (len >> 1)] = u - v >= 0 ? u - v : u - v + mod;
-        w = (w * 1 LL * wlen) % mod;
+      for (int j = 0; j < len / 2; j++) {
+        int u = a[i+j], v = (int)(1LL * a[i+j+len/2] * w % mod);
+        a[i+j] = u + v < mod ? u + v : u + v - mod;
+        a[i+j+len/2] = u - v >= 0 ? u - v : u - v + mod;
+        w = (int)(1LL * w * wlen % mod);
       }
     }
   }
