f077cacbc165e69744406841078d22117d8f0d29
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2023-10-20 09:15:58 -0500

    Update sol.cpp

--- Utilities/template/sol.cpp
+++ Utilities/template/sol.cpp
@@ -10,10 +10,10 @@ using ld = long double;
 
 
 int main(){
-  #ifdef LOCAL
-    freopen("in","r", stdin);
-    freopen("out","w",stdout);
-  #endif
+	#ifdef LOCAL
+		freopen("in","r", stdin);
+		freopen("out","w",stdout);
+	#endif
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	return 0;
 }
